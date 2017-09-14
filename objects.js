78d695bf222a8c0b3a5b46472dd244a0c4b8a707
Terfa Tortema <terfa.tortema@gmail.com>
2017-09-14 06:36:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ var updatePlaylist = function(playlist,artistName,songTitle){
 }
 
 var removeFromPlaylist = function(playlist,artistName){
-  var newPlaylist = delete playlist['artistName'];
+  var newPlaylist = delete playlist[artistName];
   return newPlaylist
 }
